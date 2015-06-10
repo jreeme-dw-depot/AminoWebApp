4549ac040e3f954f49cfd647554aa256220226fc
John Reeme <john.reeme@soteradefense.com>
2015-06-10 07:27:30 -0500

    Trying to get this baby to live up to its potential

--- server.js
+++ server.js
@@ -1,13 +1,69 @@
-var loopback = require('loopback');
-var app = module.exports = loopback();
+var debug = require('debug')('server');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-//boot(app, __dirname);
+var app = express();
 
-app.get('/', function(req,res){
-  res.send('hello world');
+// handle server listen port this way for now
+app.set('port', process.env.PORT || 3001);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(expressSession({secret: 'mySecret', resave: false, saveUninitialized: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
+app.use('/logout', require('./routes/logout'));
+app.use('/views/partials', require('./routes/partials'));
+
+app.use('/views/app', require('./routes/app/app'));
+app.use('/users', require('./routes/users'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(3001);
+// error handlers
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// and let's get things started
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
